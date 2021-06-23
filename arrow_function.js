91ffe9cf9fda00d259e28ce0d76fca42c928221f
Gouri Kanitkar <57099155+gouree99@users.noreply.github.com>
2021-06-23 12:19:05 +0530

    initial commit

---
+++ arrow_function.js
@@ -0,0 +1,23 @@
+//eg 1
+// var a = (x) => console.log(x*x);
+// a(5);
+
+//eg 2
+// var b = (x,y) => {
+//   return x*y;
+// }
+// console.log(b(2,3));
+
+//eg 3
+// var fun = (x,y) =>{
+//   console.log(2**(x*y));
+// }
+// fun(2,2)
+
+//eg 4
+// let b = (...s) => s.map(Math.sqrt);
+// console.log(b(100,25,81))
+
+//eg 5
+// const f = (p1,p2) => ({"subject" : p1, "scores" : p2});
+// console.log(f("ABC", "23"))
